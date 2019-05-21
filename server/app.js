34ad63b23832f2f54867fed8b5863a99165df7dc
Antonija Simic <antonija1023@gmail.com>
2019-05-21 16:07:59 +0200

    add schema to the middleware

--- server/app.js
+++ server/app.js
@@ -1,12 +1,16 @@
-const express = require('express');
-const graphqlHTTP = require('express-graphql');
+const express = require("express");
+const graphqlHTTP = require("express-graphql");
+const schema = require("./schema/schema");
 
 const app = express();
 
-app.use("/graphql", graphqlHTTP({
-
-}));
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema
+  })
+);
 
 app.listen(4000, () => {
-  console.log("listening")
+  console.log("listening");
 });
