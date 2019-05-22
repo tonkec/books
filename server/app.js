149850647e469fb26e39ac54fa2de8e219c489b4
Antonija Simic <antonija1023@gmail.com>
2019-05-22 22:07:58 +0200

    connect to mongo with mongoose; add dotenv for env vars

--- server/app.js
+++ server/app.js
@@ -1,8 +1,23 @@
+const dotenv = require("dotenv");
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const schema = require("./schema/schema");
+const mongoose = require("mongoose");
+dotenv.config();
 
 const app = express();
+const user = process.env.USER;
+const password = process.env.PASSWORD;
+const host = process.env.HOST;
+const port = process.env.PORT;
+const db = process.env.DB;
+
+mongoose.connect(
+  `mongodb://${user}:${password}@${host}.mlab.com:${port}/${db}`
+);
+mongoose.connection.once("open", () => {
+  console.log("connected to mongodb");
+});
 
 app.use(
   "/graphql",
