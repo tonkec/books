3a37e6e577d3b0caa19ca2dce9c6919f8d50c096
Antonija Simic <antonija1023@gmail.com>
2019-05-23 19:42:45 +0200

    remove div

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -15,11 +15,9 @@ class BookList extends Component {
   render() {
     console.log(this.props);
     return (
-      <div>
-        <ul id="book-list">
-          <li>Book name</li>
-        </ul>
-      </div>
+      <ul id="book-list">
+        <li>Book name</li>
+      </ul>
     );
   }
 }
