3bb9725e0ddbd4334532d59698e241791352a2b9
Antonija Simic <antonija1023@gmail.com>
2019-05-24 11:11:58 +0200

    add new component BookDetails

---
+++ client/src/components/BookDetails.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { graphql } from "react-apollo";
+
+class BooksDetails extends React.Component {
+  render() {
+    return (
+      <section className="books-details">
+        <p> Details of the book </p>
+      </section>
+    );
+  }
+}
