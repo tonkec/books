d1589aad36731d4b8982c6f9952f38eac5230ebe
Antonija Simic <antonija1023@gmail.com>
2019-05-24 10:18:43 +0200

    fix class to className

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,7 @@ function App() {
     <ApolloProvider client={client}>
       <main className="section">
         <div className="container">
-          <h1 class="title is-1"> Books </h1>
+          <h1 className="title is-1"> Books </h1>
           <BookList />
           <AddBook />
         </div>
