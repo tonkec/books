550d9c5f069d133681ac607e6cf9ecac429c9f4a
Antonija Simic <antonija1023@gmail.com>
2019-05-24 10:19:12 +0200

    add author, genre and name to component to component's state

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -2,43 +2,80 @@ import React from "react";
 import { getAuthorsQuery } from "./../queries/queries";
 import { graphql } from "react-apollo";
 
-const AddBook = ({ data }) => {
-  let { authors, loading } = data;
-  return (
-    <section>
-      <h2 className="title is-2"> Add New Book </h2>
-      <form>
-        <div className="field">
-          <label className="label"> Book Name </label>
-          <div className="control">
-            <input type="text" className="input" />
-          </div>
-        </div>
+class AddBook extends React.Component {
+  state = {
+    name: "",
+    genre: "",
+    author: ""
+  };
 
-        <div className="field">
-          <label className="label"> Genre </label>
-          <div className="control">
-            <input type="text" className="input" />
-          </div>
-        </div>
+  handleBookName = e => {
+    let name = e.target.value;
+    this.setState({ name });
+  };
 
-        <div className="field">
-          <label className="label"> Author </label>
-          <div className="control select">
-            {!loading && (
-              <select>
-                {authors.map(author => (
-                  <option key={author.id}> {author.name} </option>
-                ))}
-              </select>
-            )}
-          </div>
-        </div>
+  handleGenre = e => {
+    let genre = e.target.value;
+    this.setState({ genre });
+  };
 
-        <button className="button is-black"> Add Book </button>
-      </form>
-    </section>
-  );
-};
+  handleSelect = e => {
+    let author = e.target.value;
+    this.setState({ author });
+  };
+
+  handleFormSubmit = e => {
+    e.preventDefault();
+  };
+
+  render() {
+    let { authors, loading } = this.props.data;
+    return (
+      <section>
+        <h2 className="title is-2"> Add New Book </h2>
+        <form onSubmit={this.handleFormSubmit}>
+          <div className="field">
+            <label className="label"> Book Name </label>
+            <div className="control">
+              <input
+                type="text"
+                className="input"
+                onChange={this.handleBookName}
+                placeholder="Book Name"
+              />
+            </div>
+          </div>
+
+          <div className="field">
+            <label className="label"> Genre </label>
+            <div className="control">
+              <input
+                type="text"
+                className="input"
+                onChange={this.handleGenre}
+                placeholder="Genre"
+              />
+            </div>
+          </div>
+
+          <div className="field">
+            <label className="label"> Author </label>
+            <div className="control select">
+              {!loading && (
+                <select onChange={this.handleSelect}>
+                  {authors.map(author => (
+                    <option key={author.id}> {author.name} </option>
+                  ))}
+                </select>
+              )}
+            </div>
+          </div>
+
+          <button className="button is-black"> Add Book </button>
+        </form>
+      </section>
+    );
+  }
+}
 
 export default graphql(getAuthorsQuery)(AddBook);
