32a3278562ae26025e599f011fd009f1c093d94f
Antonija Simic <antonija1023@gmail.com>
2019-05-25 18:27:02 +0200

    style nav

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,13 +3,9 @@ import { Link } from "react-router-dom";
 import { withRouter } from "react-router";
 
 const Header = () => (
-  <header>
-    <Link to="/" className="ml1 no-underline black">
-      Home
-    </Link>
-    <Link to="/create" className="ml1 no-underline black">
-      Add New Book
-    </Link>
+  <header className="header">
+    <Link to="/">Home</Link>
+    <Link to="/create">Add New Book</Link>
   </header>
 );
 
