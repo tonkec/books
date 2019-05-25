e4f0a889740f8973158f4d0c5d8ca0ebcc7bd49d
Antonija Simic <antonija1023@gmail.com>
2019-05-25 12:35:46 +0200

    move files to the root of directory

---
+++ app.js
@@ -0,0 +1,34 @@
+const dotenv = require("dotenv");
+const express = require("express");
+const graphqlHTTP = require("express-graphql");
+const schema = require("./schema/schema");
+const mongoose = require("mongoose");
+dotenv.config();
+const cors = require("cors");
+const app = express();
+const user = process.env.USER;
+const password = process.env.PASSWORD;
+const host = process.env.HOST;
+const port = process.env.PORT;
+const db = process.env.DB;
+app.use(cors());
+mongoose.connect(
+  `mongodb://${user}:${password}@${host}.mlab.com:${port}/${db}`
+);
+mongoose.connection.once("open", () => {
+  console.log("connected to mongodb");
+});
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true
+  })
+);
+
+const serverPort = process.env.SERVERPORT || 4000;
+
+app.listen(serverPort, () => {
+  console.log("listening");
+});
