8ad6a9c3ab476d0e11bc0cb839e78f2431d89746
Antonija Simic <antonija1023@gmail.com>
2019-05-25 12:45:14 +0200

    add link to home

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,6 +4,9 @@ import { withRouter } from "react-router";
 
 const Header = () => (
   <header>
+    <Link to="/" className="ml1 no-underline black">
+      Home
+    </Link>
     <Link to="/create" className="ml1 no-underline black">
       Add New Book
     </Link>
