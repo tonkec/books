daaacd3287c160220b54c5d6de591526155590c7
Antonija Simic <antonija1023@gmail.com>
2019-05-25 11:58:17 +0200

    setup routes for home and add new book

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,20 @@
 import React from "react";
 import BookList from "./components/BookList";
 import AddBook from "./components/AddBook";
-import ApolloClient from "apollo-boost";
-import { ApolloProvider } from "react-apollo";
+import Header from "./components/Header";
+import { Switch, Route } from "react-router-dom";
 import "./App.scss";
 
-const client = new ApolloClient({
-  uri: "http://localhost:4000/graphql"
-});
-
-function App() {
-  return (
-    <ApolloProvider client={client}>
-      <main className="section">
-        <div className="container">
-          <h1 className="title is-1"> Books </h1>
-          <BookList />
-          <AddBook />
-        </div>
-      </main>
-    </ApolloProvider>
-  );
-}
+const App = () => (
+  <main className="section">
+    <div className="container">
+      <Header />
+      <Switch>
+        <Route exact path="/" component={BookList} />
+        <Route exact path="/create" component={AddBook} />
+      </Switch>
+    </div>
+  </main>
+);
 
 export default App;
---
+++ client/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { withRouter } from "react-router";
+
+const Header = () => (
+  <header>
+    <Link to="/create" className="ml1 no-underline black">
+      Add New Book
+    </Link>
+  </header>
+);
+
+export default Header;
